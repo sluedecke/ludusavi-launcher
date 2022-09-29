87b0e592842ef420424751b5c5ba40fb52cf5fdf
Sascha Lüdecke <sascha@meta-x.de>
2022-09-29 13:01:55 +0200

    path to tee is a variable, set popup message properly on steam deck (no window decorations)

--- ludusavi-cloud.sh
+++ ludusavi-cloud.sh
@@ -27,6 +27,7 @@ GAMENAME=`head -1 "$3/gameinfo"`
 # Standard paths
 LUDUSAVI=/usr/bin/ludusavi
 ZENITY=/usr/bin/zenity
+TEE=/usr/bin/tee
 
 # Path on steam deck if installed via flatpack
 if [ $USER == deck ]
@@ -43,10 +44,11 @@ fi
     # restore savegame
     echo $LUDUSAVI restore --force "$GAMENAME"
     (
+        echo "# Restoring savegame for $GAMENAME"
         # bypass STDOUT and explicitely redirect output to LOGFILE
-        $LUDUSAVI restore --force "$GAMENAME" 1>&2 | tee -a $LOGFILE
+        $LUDUSAVI restore --force "$GAMENAME" 1>&2 | $TEE -a $LOGFILE
     ) | $ZENITY --progress \
-               --title="Restoring $GAMENAME" \
+               --title="Savegame restore" \
                --no-cancel \
                --auto-close \
                --pulsate
@@ -59,10 +61,11 @@ fi
     # backup savegame
     echo $LUDUSAVI backup --merge --force "$GAMENAME"
     (
+        echo "# Backing up savegames for $GAMENAME"
         # bypass STDOUT and explicitely redirect output to LOGFILE
-        $LUDUSAVI backup --merge --force "$GAMENAME" 1>&2 | tee -a $LOGFILE
+        $LUDUSAVI backup --merge --force "$GAMENAME" 1>&2 | $TEE -a $LOGFILE
     ) | $ZENITY --progress \
-               --title="Backing $GAMENAME" \
+               --title="Savegame backup" \
                --no-cancel \
                --auto-close \
                --pulsate
@@ -70,4 +73,4 @@ fi
     echo End time: `date`
     echo
     echo ==================================================
-}  2>&1 | tee -a $LOGFILE
+}  2>&1 | $TEE -a $LOGFILE
