8937e8824fc574173152399ad76474cae2667ca8
Sascha Lüdecke <sascha@meta-x.de>
2022-09-29 12:37:28 +0200

    Version 0.2 - use zenity to show feedback to the user, show messages to STDOUT

--- ludusavi-cloud.sh
+++ ludusavi-cloud.sh
@@ -1,9 +1,22 @@
 #!/bin/sh
 
+# -- ABOUT --
+#
+# Wrapper script to use ludusavi for savegame backup / restoration.
+#
+# Assumes that $3 is the directory with the game install which contains the file
+# `gameinfo`.
+#
 # This file comes without warranty, use at your own risk!
 #
 # License: MIT License
 # Author: Sascha Lüdecke <sascha@meta-x.de>
+
+# -- HISTORY --
+#
+# Version: 0.2 - 2022-09-29
+#
+# . use zenity to tell user that we back up / restore
 #
 # Version: 0.1 - 2022-09-27
 
@@ -11,8 +24,9 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 LOGFILE=$SCRIPT_DIR/ludusavi-cloud.log
 GAMENAME=`head -1 "$3/gameinfo"`
 
-# Linux installation
+# Standard paths
 LUDUSAVI=/usr/bin/ludusavi
+ZENITY=/usr/bin/zenity
 
 # Path on steam deck if installed via flatpack
 if [ $USER == deck ]
@@ -24,22 +38,36 @@ fi
     echo ==================================================
     echo
     echo Gamename is: $GAMENAME
-    
+    echo Start time: `date`
+
     # restore savegame
-    echo Restore savegame
     echo $LUDUSAVI restore --force "$GAMENAME"
-    $LUDUSAVI restore --force "$GAMENAME"
-    
+    (
+        # bypass STDOUT and explicitely redirect output to LOGFILE
+        $LUDUSAVI restore --force "$GAMENAME" 1>&2 | tee -a $LOGFILE
+    ) | $ZENITY --progress \
+               --title="Restoring $GAMENAME" \
+               --no-cancel \
+               --auto-close \
+               --pulsate
+
     # run game
     echo Game run command:
     echo $@
     "$@"
-    
-    # backup savegame
-    echo Backup savegame
-    echo $LUDUSAVI backup --merge --force "$GAMENAME"
-    $LUDUSAVI backup --merge --force "$GAMENAME"
 
+    # backup savegame
+    echo $LUDUSAVI backup --merge --force "$GAMENAME"
+    (
+        # bypass STDOUT and explicitely redirect output to LOGFILE
+        $LUDUSAVI backup --merge --force "$GAMENAME" 1>&2 | tee -a $LOGFILE
+    ) | $ZENITY --progress \
+               --title="Backing $GAMENAME" \
+               --no-cancel \
+               --auto-close \
+               --pulsate
+
+    echo End time: `date`
     echo
     echo ==================================================
-} >> $LOGFILE 2>&1
+}  2>&1 | tee -a $LOGFILE
